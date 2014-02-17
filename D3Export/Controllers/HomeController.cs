857a3db612677959048689d3666785f935a1004e
Jarrett Meyer <jarrettmeyer@gmail.com>
2014-02-17 08:07:56 -0500

    refactoring out SVG writer

--- D3Export/Controllers/HomeController.cs
+++ D3Export/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Globalization;
 using System.IO;
 using System.Text;
 using System.Web.Mvc;
+using D3Export.Lib;
 using D3Export.ViewModels;
 
 namespace D3Export.Controllers
@@ -18,34 +19,15 @@ namespace D3Export.Controllers
         [ValidateInput(false)]
         public ActionResult Save(PostSvg form)
         {
-            var filePath = GetRandomFilePath();
-            using (var file = new FileStream(filePath, FileMode.Create))
+            using (var svgWriter = new SVGWriter())
             {
-                byte[] buffer = Encoding.UTF8.GetBytes(form.SVG);
-                int length = buffer.Length;
-                file.Write(buffer, 0, length);
+                svgWriter.Write(form.SVG);
             }
 
             return Json(new
             {
                 timestamp = DateTime.UtcNow.ToString(CultureInfo.InvariantCulture)
             });
-        }
-
-        public static string Extension
-        {
-            get { return ".svg"; }
-        }
-
-        public string Folder
-        {
-            get { return Server.MapPath("~/Content/Images"); }
-        }
-
-        public string GetRandomFilePath()
-        {
-            var filename = Guid.NewGuid().ToString("N") + Extension;
-            return Path.Combine(Folder, filename);
         }
 	}
 }
---
+++ D3Export/Lib/SVGWriter.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Web;
+
+namespace D3Export.Lib
+{
+    public class SVGWriter : IDisposable
+    {
+        private const string EXTENSION = ".svg";
+        private readonly FileStream fileStream;
+
+        public SVGWriter(string filenameWithoutExtension = null, string saveDirectory = null)
+        {
+            filenameWithoutExtension = filenameWithoutExtension ?? Guid.NewGuid().ToString("N");
+            saveDirectory = saveDirectory ?? HttpContext.Current.Server.MapPath("~/Content/Images");
+            var filename = filenameWithoutExtension + EXTENSION;
+            var path = Path.Combine(saveDirectory, filename);
+            fileStream = new FileStream(path, FileMode.Create);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        public void Write(string svgContent)
+        {
+            byte[] buffer = Encoding.UTF8.GetBytes(svgContent);
+            int length = buffer.Length;
+            fileStream.Write(buffer, 0, length);
+        }
+
+        protected virtual void Dispose(bool isDisposing)
+        {
+            if (isDisposing)
+            {
+                if (fileStream != null)
+                {
+                    fileStream.Dispose();
+                }
+            }
+        }
+    }
+}
